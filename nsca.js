9b4a32a61257842298e668b7fec443a855b1b19c
jmoss <j.moss@nedeco.de>
2016-11-02 12:40:08 +0100

    Revert "remove mcrypt dependency"
    This reverts commit 638aa8920e0da35ac9c130c74b889be44f17cf15.

--- nsca.js
+++ nsca.js
@@ -1,6 +1,6 @@
 var net = require("net");
 var crc32 = require("buffer-crc32");
-// var Crypter = require("./lib/crypto");
+var Crypter = require("./lib/crypto");
 
 
 function Notifier(host, port, secret, encryption) {
@@ -19,17 +19,17 @@ Notifier.prototype.send = function(hostName, serviceDesc, statusCode, pluginOutp
   var client = new net.Socket();
 
   client.connect(this.port, this.host, function() {
-    // console.log("Connected to: " + this.host + ":" + this.port);
+    //console.log("Connected to: " + this.host + ":" + this.port);
   }.bind(this));
 
   client.on("data", function(data) {
-    // console.log("data received: " + data);
+    //console.log("data received: " + data);
     var encoding = "binary";
     var inBuffer = new Buffer(data);
-    // var iv = inBuffer.toString(encoding, 0, 128);
-    // console.log("received IV: " + iv);
+    var iv = inBuffer.toString(encoding, 0, 128);
+    //console.log("received IV: " + iv);
     var timestamp = inBuffer.readInt32BE(128);
-    // console.log("received timestamp: " + timestamp);
+    //console.log("received timestamp: " + timestamp);
 
     var outBuffer = new Buffer(MSG_LENGTH);
     // empty Buffer
@@ -40,7 +40,7 @@ Notifier.prototype.send = function(hostName, serviceDesc, statusCode, pluginOutp
     outBuffer.fill("h", 2, 3);
     // CRC32 null
     outBuffer.writeUInt32BE(0, 4);
-    // timestamp
+    //timestamp
     outBuffer.writeUInt32BE(timestamp, 8);
     // status code
     outBuffer.writeInt16BE(statusCode, 12);
@@ -53,20 +53,22 @@ Notifier.prototype.send = function(hostName, serviceDesc, statusCode, pluginOutp
     // CEC32
     outBuffer.writeUInt32BE(crc32.unsigned(outBuffer), 4);
 
-    // if (this.encryption) {
-    //   var encrypter = new Crypter(this.encryption, this.secret, iv);
-    //   outBuffer = encrypter.encode(outBuffer);
-    // }
+    if (this.encryption) {
+      var encrypter = new Crypter(this.encryption, this.secret, iv);
+      outBuffer = encrypter.encode(outBuffer);
+    }
 
 
     client.write(outBuffer, function(a) {
       client.destroy();
-      // console.log("data sent: " + outBuffer);
+      //console.log("data sent: " + outBuffer);
     });
+
+
   }.bind(this));
 
   client.on("close", function() {
-    // no errors, lets go!
+    //no errors, lets go!
     callback(null);
   }.bind(this));
 
@@ -74,6 +76,7 @@ Notifier.prototype.send = function(hostName, serviceDesc, statusCode, pluginOutp
     var err = new Error("NSCA server connection failed!");
     callback(err);
   }.bind(this));
+
 };
 
 
@@ -88,30 +91,30 @@ module.exports = {
 
   // encryption types
   ENCRYPT_NONE       : 0,  // no encryption
-  // ENCRYPT_XOR        : 1,  // not really encrypted, just obfuscated
-  // ENCRYPT_DES        : 2,  // DES
-  // ENCRYPT_3DES       : 3,  // 3DES or Triple DES
-  // ENCRYPT_CAST128    : 4,  // CAST-128
-  // ENCRYPT_CAST256    : 5,  // CAST-256
-  // ENCRYPT_XTEA       : 6,  // xTEA
-  // ENCRYPT_3WAY       : 7,  // 3-WAY
-  // ENCRYPT_BLOWFISH   : 8,  // SKIPJACK
-  // ENCRYPT_TWOFISH    : 9,  // TWOFISH
-  // ENCRYPT_LOKI97     : 10, // LOKI97
-  // ENCRYPT_RC2        : 11, // RC2
-  // ENCRYPT_ARCFOUR    : 12, // RC4
-  // ENCRYPT_RC6        : 13, // RC6            (UNUSED)
-  // ENCRYPT_RIJNDAEL128: 14, // RIJNDAEL-128
-  // ENCRYPT_RIJNDAEL192: 15, // RIJNDAEL-192
-  // ENCRYPT_RIJNDAEL256: 16, // RIJNDAEL-256
-  // ENCRYPT_MARS       : 17, // MARS           (UNUSED)
-  // ENCRYPT_PANAMA     : 18, // PANAMA         (UNUSED)
-  // ENCRYPT_WAKE       : 19, // WAKE
-  // ENCRYPT_SERPENT    : 20, // SERPENT
-  // ENCRYPT_IDEA       : 21, // IDEA           (UNUSED)
-  // ENCRYPT_ENIGMA     : 22, // ENIGMA (Unix crypt)
-  // ENCRYPT_GOST       : 23, // GOST
-  // ENCRYPT_SAFER64    : 24, // SAFER-sk64
-  // ENCRYPT_SAFER128   : 25, // SAFER-sk128
-  // ENCRYPT_SAFERPLUS  : 26, // SAFER+
+  ENCRYPT_XOR        : 1,  // not really encrypted, just obfuscated
+  ENCRYPT_DES        : 2,  // DES
+  ENCRYPT_3DES       : 3,  // 3DES or Triple DES
+  ENCRYPT_CAST128    : 4,  // CAST-128
+  ENCRYPT_CAST256    : 5,  // CAST-256
+  ENCRYPT_XTEA       : 6,  // xTEA
+  ENCRYPT_3WAY       : 7,  // 3-WAY
+  ENCRYPT_BLOWFISH   : 8,  // SKIPJACK
+  ENCRYPT_TWOFISH    : 9,  // TWOFISH
+  ENCRYPT_LOKI97     : 10, // LOKI97
+  ENCRYPT_RC2        : 11, // RC2
+  ENCRYPT_ARCFOUR    : 12, // RC4
+  ENCRYPT_RC6        : 13, // RC6            (UNUSED)
+  ENCRYPT_RIJNDAEL128: 14, // RIJNDAEL-128
+  ENCRYPT_RIJNDAEL192: 15, // RIJNDAEL-192
+  ENCRYPT_RIJNDAEL256: 16, // RIJNDAEL-256
+  ENCRYPT_MARS       : 17, // MARS           (UNUSED)
+  ENCRYPT_PANAMA     : 18, // PANAMA         (UNUSED)
+  ENCRYPT_WAKE       : 19, // WAKE
+  ENCRYPT_SERPENT    : 20, // SERPENT
+  ENCRYPT_IDEA       : 21, // IDEA           (UNUSED)
+  ENCRYPT_ENIGMA     : 22, // ENIGMA (Unix crypt)
+  ENCRYPT_GOST       : 23, // GOST
+  ENCRYPT_SAFER64    : 24, // SAFER-sk64
+  ENCRYPT_SAFER128   : 25, // SAFER-sk128
+  ENCRYPT_SAFERPLUS  : 26, // SAFER+
 };
